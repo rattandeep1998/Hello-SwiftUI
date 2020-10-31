ec869f96282c4739abc53800213f20a681877827
RATTANDEEP SINGH <rattandeep_singh@intuit.com>
2020-10-31 17:27:30 +0530

    Initial Commit

---
+++ Hello-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello-SwiftUI
+//
+//  Created by RATTANDEEP SINGH on 31/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello-SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello-SwiftUI
+//
+//  Created by RATTANDEEP SINGH on 31/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
